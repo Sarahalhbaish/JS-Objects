c9d63b79755bc471c9b0297712e29f63980efafb
Sarahalhbaish <sarah21995@live.com>
2024-11-04 19:15:07 +0300

    Task Complete

---
+++ index.js
@@ -0,0 +1,50 @@
+/* --------- Part 1 --------- */
+/* --------- Task 1 --------- */
+const book = {
+    title: "JavaScript: The Definitive Guide",
+    author: "David Flanagan",
+    publishedYear: 2020,
+    genre: "Programming"
+}
+
+
+/* --------- Task 2 --------- */
+// console.log(book.title); 
+// console.log(book["publishedYear"]); 
+
+
+/* ------------------------------------------------------------ */
+
+
+/* --------- Part 2 --------- */
+/* --------- Task 3 --------- */
+book.pageCount = 1096;
+
+
+/* --------- Task 4 --------- */
+book.ISBN = "978-1491952023";
+
+
+/* --------- Task 5 --------- */
+book.publishedYear = 2021;
+
+
+/* ------------------------------------------------------------ */
+
+
+/* --------- Part 3 --------- */
+/* --------- Task 6 --------- */
+book.author = ["David Flanagan", "Anders Hejlsberg"];
+
+
+/* --------- Task 7 --------- */
+book.reviews = {
+    reviewer: "Book Critic",
+    comment: "A comprehensive guide to JavaScript."
+};
+
+
+/* ------------------------------------------------------------ */
+
+
+console.log(book);
